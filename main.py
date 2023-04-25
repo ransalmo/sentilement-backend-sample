a8b1eed5e4b8ebf862911509830ee503e46d09f1
ransalmo <randysalas2@me.com>
2023-04-25 14:49:22 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from transformers import pipeline
+from fastapi import FastAPI
+
+from message import Message
+
+sentiment_pipeline = pipeline(model="nlptown/bert-base-multilingual-uncased-sentiment")
+app = FastAPI()
+
+
+@app.get('/')
+def get_root():
+    return {'Este es el backend de una aplicación de análisis de sentimientos'}
+
+
+@app.get('/analysis/')
+async def query_analysis(text: str):
+    return analyze_sentiment(text)
+
+
+@app.post('/analysispost/')
+async def query_analysis_post(content: Message):
+    return analyze_sentiment(content.content)
+
+
+def analyze_sentiment(text: str):
+    """Get and process result"""
+    result = sentiment_pipeline(text)
+    sent = ''
+    if result[0]['label'] == '1 star':
+        sent = 'Muy negativo'
+    elif result[0]['label'] == '2 star':
+        sent = 'Negativo'
+    elif result[0]['label'] == '3 stars':
+        sent = 'Neutral'
+    elif result[0]['label'] == '4 stars':
+        sent = 'Positivo'
+    else:
+        sent = 'Muy positivo'
+    prob = result[0]['score']
+    # Format and return results
+    return {'sentiment': sent, 'probability': prob, 'text': text}
